34b4ae1e8405866345b6d3a02e08cc480e97b729
Ashutosh <anandashutosh258@gmail.com>
2024-05-18 04:18:16 +0530

    few changes before deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import {v2 as cloudinary} from 'cloudinary'
@@ -21,16 +22,30 @@ cloudinary.config({
 
 const app=express();
 const PORT=process.env.PORT || 5000;
+const __dirname=path.resolve();
+
 
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
 
 
+
+
+
 app.use("/api/auth",authRoute);
 app.use("/api/user",userRoute);
 app.use("/api/post",postRoute);
 app.use("/api/notification",notificationRoute);
+
+
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    })
+}
 
 
 app.listen(PORT,()=>{
